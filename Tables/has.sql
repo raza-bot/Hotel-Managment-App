9a6a73ed02a53ccb95849f27e5eb1ba5d991d482
Kevin Pham <phambamkevin@gmail.com>
2019-10-14 21:01:48 -0700

    has relationship table

---
+++ Tables/has.sql
@@ -0,0 +1,18 @@
+CREATE TABLE has (hotelID TINYINT, userID TINYINT);
+
+INSERT INTO has(hotelID, userID) VALUES(1, 2);
+INSERT INTO has(hotelID, userID) VALUES(2, 13);
+INSERT INTO has(hotelID, userID) VALUES(3, 5);
+INSERT INTO has(hotelID, userID) VALUES(4, 1);
+INSERT INTO has(hotelID, userID) VALUES(5, 8);
+INSERT INTO has(hotelID, userID) VALUES(6, 2);
+INSERT INTO has(hotelID, userID) VALUES(7, 4);
+INSERT INTO has(hotelID, userID) VALUES(8, 8);
+INSERT INTO has(hotelID, userID) VALUES(9, 2);
+INSERT INTO has(hotelID, userID) VALUES(10, 3);
+INSERT INTO has(hotelID, userID) VALUES(11, 9);
+INSERT INTO has(hotelID, userID) VALUES(12, 7);
+INSERT INTO has(hotelID, userID) VALUES(13, 1);
+INSERT INTO has(hotelID, userID) VALUES(14, 4);
+INSERT INTO has(hotelID, userID) VALUES(15, 6);
+INSERT INTO has(hotelID, userID) VALUES(16, 8);
