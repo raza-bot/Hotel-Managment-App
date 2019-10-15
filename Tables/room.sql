36bd3a3a05f5f30524f949a6345fbe879a512719
Kevin Pham <phambamkevin@gmail.com>
2019-10-14 20:19:24 -0700

    added script for room table

---
+++ Tables/room.sql
@@ -0,0 +1,18 @@
+CREATE TABLE room (hotelID TINYINT, roomNum TINYINT, type VARCHAR(20), status VARCHAR(20));
+
+INSERT INTO room(hotelID, roomNum, type, status) VALUES('1', '001', 'Single', 'Available');
+INSERT INTO room(hotelID, roomNum, type, status) VALUES('2', '001', 'Double', 'Available');
+INSERT INTO room(hotelID, roomNum, type, status) VALUES('3', '001', 'Triple', 'Available');
+INSERT INTO room(hotelID, roomNum, type, status) VALUES('4', '001', 'Suite', 'Available');
+INSERT INTO room(hotelID, roomNum, type, status) VALUES('5', '001', 'Single', 'Booked');
+INSERT INTO room(hotelID, roomNum, type, status) VALUES('6', '001', 'Triple', 'Booked');
+INSERT INTO room(hotelID, roomNum, type, status) VALUES('7', '001', 'Suite', 'Available');
+INSERT INTO room(hotelID, roomNum, type, status) VALUES('8', '001', 'Double', 'Available');
+INSERT INTO room(hotelID, roomNum, type, status) VALUES('9', '002', 'Single', 'Booked');
+INSERT INTO room(hotelID, roomNum, type, status) VALUES('10', '010', 'Triple', 'Booked');
+INSERT INTO room(hotelID, roomNum, type, status) VALUES('11', '008', 'Double', 'Available');
+INSERT INTO room(hotelID, roomNum, type, status) VALUES('12', '013', 'Double', 'Booked');
+INSERT INTO room(hotelID, roomNum, type, status) VALUES('13', '004', 'Single', 'Available');
+INSERT INTO room(hotelID, roomNum, type, status) VALUES('14', '008', 'Suite', 'Available');
+INSERT INTO room(hotelID, roomNum, type, status) VALUES('15', '010', 'Double', 'Booked');
+INSERT INTO room(hotelID, roomNum, type, status) VALUES('16', '009', 'Triple', 'Booked');
