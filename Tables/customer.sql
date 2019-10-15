7308b70ab38b7b824de611f8fa20948153b76f4c
raza <ahmad.abdali001@gmail.com>
2019-10-15 13:35:35 -0700

    Customer and reserver tables are added

---
+++ Tables/customer.sql
@@ -0,0 +1,3 @@
+CREATE TABLE customer(user_id INT PRIMARY KEY, reserveNum SMALLINT); 
+INSERT INTO customer VALUES(1, 001),(2, 002), (3, 000),(4, 010),
+(5, 011),(6, 009),(7, 005),(8, 012),(9, 001),(10, 007),(11, 009),(12, 015),(13, 011),(14, 002), (15, 010); 
