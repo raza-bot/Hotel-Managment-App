537bfbae1f6a48d938f4256d732847af26a5e0b7
Bronsin Benyamin <bronsinb@gmail.com>
2019-11-01 13:41:08 -0700

    Updated room.sql

--- Tables/room.sql
+++ Tables/room.sql
@@ -1,18 +1,5 @@
-CREATE TABLE room (hotelID TINYINT, roomNum TINYINT, type VARCHAR(20), status VARCHAR(20));
+CREATE TABLE Room (hotelID INT, roomNum SMALLINT, type VARCHAR(20), status boolean);
 
-INSERT INTO room(hotelID, roomNum, type, status) VALUES('1', '001', 'Single', 'Available');
-INSERT INTO room(hotelID, roomNum, type, status) VALUES('2', '001', 'Double', 'Available');
-INSERT INTO room(hotelID, roomNum, type, status) VALUES('3', '001', 'Triple', 'Available');
-INSERT INTO room(hotelID, roomNum, type, status) VALUES('4', '001', 'Suite', 'Available');
-INSERT INTO room(hotelID, roomNum, type, status) VALUES('5', '001', 'Single', 'Booked');
-INSERT INTO room(hotelID, roomNum, type, status) VALUES('6', '001', 'Triple', 'Booked');
-INSERT INTO room(hotelID, roomNum, type, status) VALUES('7', '001', 'Suite', 'Available');
-INSERT INTO room(hotelID, roomNum, type, status) VALUES('8', '001', 'Double', 'Available');
-INSERT INTO room(hotelID, roomNum, type, status) VALUES('9', '002', 'Single', 'Booked');
-INSERT INTO room(hotelID, roomNum, type, status) VALUES('10', '010', 'Triple', 'Booked');
-INSERT INTO room(hotelID, roomNum, type, status) VALUES('11', '008', 'Double', 'Available');
-INSERT INTO room(hotelID, roomNum, type, status) VALUES('12', '013', 'Double', 'Booked');
-INSERT INTO room(hotelID, roomNum, type, status) VALUES('13', '004', 'Single', 'Available');
-INSERT INTO room(hotelID, roomNum, type, status) VALUES('14', '008', 'Suite', 'Available');
-INSERT INTO room(hotelID, roomNum, type, status) VALUES('15', '010', 'Double', 'Booked');
-INSERT INTO room(hotelID, roomNum, type, status) VALUES('16', '009', 'Triple', 'Booked');
+INSERT INTO Room(hotelID, roomNum, type, status) VALUES('1', 1, 'Single', true);
+INSERT INTO Room(hotelID, roomNum, type, status) VALUES('2', 1, 'Double', false);
+INSERT INTO Room(hotelID, roomNum, type, status) VALUES('3', 1, 'Triple', true);
